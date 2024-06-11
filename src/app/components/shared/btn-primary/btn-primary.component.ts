5317ba0f1696c9e66f4e6214a2206ac32569427e
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-06-10 22:56:21 -0300

    add: primary button component

---
+++ src/app/components/shared/btn-primary/btn-primary.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'btn-primary',
+  templateUrl: './btn-primary.component.html',
+  styleUrl: './btn-primary.component.scss'
+})
+export class BtnPrimaryComponent {
+  @Input() text: string = '';
+}
