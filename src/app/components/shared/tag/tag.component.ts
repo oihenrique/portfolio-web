909bbaae9c5be05bd9f7dddd046ef9689c77ff96
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-06-13 20:46:44 -0300

    feat: add tag component

---
+++ src/app/components/shared/tag/tag.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html',
+  styleUrl: './tag.component.scss'
+})
+export class TagComponent {
+  @Input() name: string = '';
+  @Input() color: string = '';
+}
