f3ffe4daec09c96562523cf7873a76978a419564
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-06-20 20:16:11 -0300

    add: susent-hotel page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UniformixComponent } from './components/projects/uniformix/uniformix.component';
+import { SunsetHotelComponent } from './components/projects/sunset-hotel/sunset-hotel.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'projects/uniformix', component: UniformixComponent}
+  { path: 'projects/uniformix', component: UniformixComponent},
+  { path: 'projects/sunsetHotel', component: SunsetHotelComponent}
 ];
 
 @NgModule({
