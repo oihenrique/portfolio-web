6aaeaceeca1c3abf1d06e425a8def24f95e9bd54
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-06-19 21:39:00 -0300

    add: uniformix page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UniformixComponent } from './components/projects/uniformix/uniformix.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'projects/uniformix', component: UniformixComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/projects/uniformix/uniformix.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uniformix',
+  templateUrl: './uniformix.component.html',
+  styleUrl: './uniformix.component.scss'
+})
+export class UniformixComponent {
+
+}
