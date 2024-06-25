3a8c3442a917bc66e3e3ac100f0c74347ce67125
oi.henriquegomes <oi.henriquegomes@gmail.com>
2024-06-25 23:18:16 +0000

    add: automatedReports page

---
+++ src/app/components/projects/automated-reports/automated-reports.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-automated-reports',
+  templateUrl: './automated-reports.component.html',
+  styleUrl: './automated-reports.component.scss'
+})
+export class AutomatedReportsComponent {
+
+}
